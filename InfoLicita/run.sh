94c454e9e5761ebb62811bea630bf2b5029f6d17
ruben <rubenglezant@gmail.com>
2015-12-20 20:09:14 +0100

    first commit

--- InfoLicita/run.sh
+++ InfoLicita/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-FILES=./*xls*
+FILES=*xls*
 rm -Rf salida.txt
 for f in $FILES
 do
   echo "Processing $f file..."
-  python excel.py > out.txt
-  python ajusta.py >> salida.txt
+  ssconvert -O 'separator=| format=raw' "$f" out.txt
+  cat out.txt >> salida.txt
 done
+cat salida.txt | sort | uniq > salidaSortUniq.txt
 
